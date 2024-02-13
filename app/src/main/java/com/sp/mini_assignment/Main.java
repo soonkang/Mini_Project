8c693f87bb0db919f03349affac725a2959cc6e8
Sk <soonkang.22@ichat.sp.edu.sg>
2024-02-13 23:44:16 +0800

    Add button to select date and time and Paypal payment

--- app/src/main/java/com/sp/mini_assignment/Main.java
+++ app/src/main/java/com/sp/mini_assignment/Main.java
@@ -2,6 +2,7 @@ package com.sp.mini_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Main extends AppCompatActivity {
 
@@ -10,18 +11,26 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Directly switch to Fragment_Point_System when the activity starts
-        switchToFragmentProfile();
+        // Directly switch to Fragment_feedback when the activity starts
+        switchToFragmentFeedback(null); // Passing null as there are no card details initially
     }
 
-    // Method to switch to Fragment_Point_System
-    public void switchToFragmentProfile() {
-        // Create an instance of Fragment_Point_System
-        fragment_profile fragmentProfile = new fragment_profile();
+    // Method to switch to Fragment_feedback with card details
+    public void switchToFragmentFeedback(String cardDetails) {
+        Log.d("Main", "Switching to Fragment_feedback");
 
-        // Replace the current fragment with Fragment_Point_System
+        // Create an instance of Fragment_feedback
+        Fragment_fedback fragmentFeedback = new Fragment_fedback();
+
+        // Pass card details to Fragment_feedback using Bundle
+        Bundle bundle = new Bundle();
+        bundle.putString("cardDetails", cardDetails);
+        fragmentFeedback.setArguments(bundle);
+
+        // Replace the current fragment with Fragment_feedback
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, fragmentProfile)
+                .replace(R.id.fragment_container, fragmentFeedback)
+                .addToBackStack(null)
                 .commit();
     }
 }
